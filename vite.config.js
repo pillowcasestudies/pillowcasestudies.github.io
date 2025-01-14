e3e1c1d0602073627f331788f19fb5c896196725
Barry Weinstein <pillowcasestudies@gmail.com>
2025-01-14 17:50:57 -0500

    again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import html from '@rollup/plugin-html';
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 4173,  // Use the PORT from Heroku, fallback to 4173 for local development
+    host: '0.0.0.0',  // Ensure the app binds to all available network interfaces
+  },
   root: './',
   plugins: [react()],
 })
