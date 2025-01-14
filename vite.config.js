41d06e719c81d3ba9507bfc7b120602c614b2a97
Barry Weinstein <pillowcasestudies@gmail.com>
2025-01-14 17:44:38 -0500

    again

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import html from '@rollup/plugin-html';
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: 'public/index.html' // Ensures Rollup knows where the entry HTML file is
-    },
-    outDir: 'dist', // This is where the production files will be output
-  },
-  publicDir: 'public',
+  root: './',
+  plugins: [react()],
 })
